c9213713820a6b423e0959f9f2a79acbc71462d8
kkc <kkc@192.168.123.105>
2022-12-20 22:41:50 +0900

    구직사이트내용 크롤링이 잘 안되니 음원사이트내용 커스텀해서 해보자 현재 아이유 url 썸네일 등을 가져온다. 목록 view를 만들어 카드형태로 썸네일과 url 제목등등을 보여주게 해보자 그 후 가수 이름을 입력해서 크롤링 할 수 있게 바꿔보자

--- project_portfolio/src/com/test/controller/CrawlingController_test.java
+++ project_portfolio/src/com/test/controller/CrawlingController_test.java
@@ -1,9 +1,6 @@
 package com.test.controller;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Iterator;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -16,22 +13,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.test.service.CrawlingService;
 
 @Controller
-public class CrawlingController {
+public class CrawlingController_test {
 	@Autowired
 	private CrawlingService crawlingService;
-
-	@GetMapping(value = "crawling1")
+	
+	@GetMapping(value = "crawling2")
 	public void Crawling() throws IOException {
 		// Jsoup를 이용해서 뮤직비디오 크롤링
 		String name = "아이유"; // 검색할 Artist
 		String url = "https://www.genie.co.kr/search/searchMv?query=" + name;// 크롤링할 url
 		Document doc = null;
 
-		try {
+		try
+		{
 
 			doc = Jsoup.connect(url).get(); // Document에는 페이지의 전체 소스가 저장된다
 
-		} catch (IOException e) {
+		}catch(
+		IOException e)
+		{
 
 			e.printStackTrace();
 
@@ -42,11 +42,13 @@ public class CrawlingController {
 		Elements element = doc.select("tr.list"); // MV 리스트가 있는 태그
 		String defaulturl = "https://www.genie.co.kr/detail/mediaInfo?xvnm=";
 
-		for (Element element1 : element) {
+		for(
+		Element element1:element)
+		{
 			System.out.println("제목 : " + element1.select("td a").attr("title")); // 제목 크롤링
 			System.out.println("url : " + defaulturl + element1.attr("mvid")); // url id 크롤링
 			System.out.println("썸네일 : " + element1.select("td a img").attr("src") + "\n"); // 썸네일 크롤링
 		}
 	}
-
+	
 }
